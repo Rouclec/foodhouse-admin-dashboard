0fc89f900fcbd1e494e0a824fa6baa61b02b3cf1
Rouclec <senatorasonganyi97@gmail.com>
2025-04-07 16:56:48 +0100

    ignore token for public endpoints

--- jsonproxy/interceptors/firebase.go
+++ jsonproxy/interceptors/firebase.go
@@ -3,6 +3,7 @@ package interceptors
 import (
 	"context"
 	"net/http"
+	"strings"
 
 	"firebase.google.com/go/auth"
 )
@@ -17,6 +18,13 @@ type TokenVerifier interface {
 func NewFirebaseAuthenticationInterceptor(client TokenVerifier) HTTPInterceptor {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			
+			// Skip token verification for public routes
+			if strings.HasPrefix(r.URL.Path, "/v1/public") {
+				next.ServeHTTP(w, r)
+				return
+			}
+			
 			// Extract the Firebase ID token from the Authorization header
 			idToken := r.Header.Get(HeaderAuthentication)
 
