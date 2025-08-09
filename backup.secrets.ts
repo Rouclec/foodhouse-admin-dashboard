c24ff309ce2cbb01c8873294eacc9ccd06a3a243
Rouclec <senatorasonganyi97@gmail.com>
2025-08-09 23:02:18 +0100

    forcing deployment

---
+++ backup.secrets.ts
@@ -0,0 +1,32 @@
+// SMTP SECRETS 
+{"password":"0BEB64871540E5F02C3AE5E88F6B03DA436E","port":"2525","host":"smtp.elasticemail.com","username":"info@myfoodhouse.com"}
+
+// Company SECRETS
+{"password":"L4JbufqiqWQZrHY1qHDWHpbWIKRRR4","phone":"+237691477330","email":"info@myfoodhouse.com"}
+
+// Nexah client SECRETS
+{"password":"nexah2024","base_url":"https://smsvas.com","user":"husseinleong@gmail.com","sender_id":"FOOD HOUSE"}
+
+// Firebase_sdk SECRETS
+{"password":"63JyflfsRztlEo0DRYdGHqI9eUoLjX","apiKey":" AIzaSyDg_WnTLJD_NOBs8_xmZJB8pIyoVALOHgo"}
+
+// campay secrets
+{"tpw_secret_key":"40030fe678110837377ab91c375984f97c2b84e3b86fe78000999e87bca28ad6","tpw_app_token":"a65ba270-0d83-4a13-a6f1-d7e57c394ad0","password":"H9J1RXXegA8OXek20jfuChGK4pN45A","tpw_webhook":"https://api.foodhousetesting.xyz/v1/public/confirm-payment","base_url":"placeholder","tpw_base_url":"https://mobilewallet.trustpayway.com","username":"placeholder"}
+
+// firebase service account
+{
+    "type": "service_account",
+    "project_id": "foodhouse-int",
+    "private_key_id": "ae44d0a1f039cd57049d175016fa5fcf53897a0d",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC2crSabxfg0H9s\npAbHlGuihtzYy2hMzbsKHpDgMwQfK/4WrTvXMq3Z7fM3sTZ2CIsxs1+XulF3Djkx\nxiqEu9JSfrpSmqwKUFRwX9ky5TKD3SXltPcH10t3G1HbKqy2R2CK28eQ/5Kg06F0\nHigo12qJX3OImeCqkUhMqSD5UwhJzThjtSGDuA4DOP2hVrs0YVcn5NWYvpzMzJVg\nltYpk19pkpSdiUe4HkzsFvQNjyD9Y3o/9W75bEBPOZ9vUxvymM3XYcgVwLhMSbIC\nuwMW6rFPuNLbBrVz6fhMs8FlNm8HxnkNUoRbwrkRGjmCqKA1TP5MY58iZRAeXrlB\n2xzG6jCJAgMBAAECggEAD2GKMj7NX4mURH9rhuXPUStXcPts9pSdgRPkmNIUNeCK\nkjTWh6ThHS8/QuGwurCrs2vw0iTF+2h6j+Sp7vi6mXPPl2ksuc3d4H9kGzNRV+aL\n7FqePM38sbTe6MAnQVKxI+O1Zlb1cJOG1/g24IKQm7YtUkXOt/J5iuTzP7GtUDNV\nVR26srxnYh4BjUlqKwFh3AhWxxsi8El68OKIOx57C/SANcTA6HAQfLOmn3rRBDZX\nd6NeYr7ox/skSABoF5dZ8d1lWg54xgv3PMl0oF9WY6oJwlIQ29zwlN3XJPdcbR4s\noBKwmkaHmAeREHp0VQCbmpr+GTOR9xh8gbrpKhDlJQKBgQDoi6xiGJOD6oF+p9T8\nQBSD4pG5zJqiGYDjLpu/LJKH3ASlQmZhkzvT/CWaEPwcIKMJgurWb43sJrAne15E\nDK6u7ffqJHH5fdaFjbFcao/kJygri2PVfcLWRRuMx+4YlJpVxsEKb19Fs+1vGowf\nXpi5XLu6uDF/4IRGykf7i9TirwKBgQDI2YOXChEG5gIoISvGEgWFr+K1hAwpP0zY\nCKyvlchaJpKzJ5E8vCtQc5LcFrutkO5rXVI6eTh40HVBIdel5yUgYmyjE46mId1c\nJa4qvEupM9nXzpoQG1Ke6TnGTNJlt9vxr5hBlPLREszTq+voJOuQoyz6yRxLrnju\nQ4V3RpJORwKBgQDV0jf7KuXY0fOn5eIs3y795YJg+QTmL9UXD3Ad5HoHegK7gcZp\nydulG5/nfd6Sbl/7Cumrqn8wuJIYe5M9tvuX1S+zlzdFVLO/sX13oHQWMPr6rvxo\n/W39XyXp2JvQ9jLC1DIDykwr5zlWg258GZsMNXxYP60kC+kUvkJZxUVF+QKBgDaL\ni8cK/tDGyjMnvwGNh15lA8aT09Gn1TCeOnd8z+pbq19oEXNTDthMasfGjLV4NKeH\nsnsazJFS22US0cPGYDjJQgOPnh/T9o+90+zbMhkDcde/zest4GQzpnMKDeF96wdZ\nzpmQ39iTqHibqfdjf23KQr7guwR6n3DleWuJCNalAoGAcTaTZj50O5JEfWILf3Zn\nhh94HJCNPZCGon6IW6BzIpdaZghmMmOM7VEhNocuhRwGrnxvD8VIkmHWxHPByvB8\nEwZEzeW4GWeUWfpTmoWmGrKQ7xKJxwP66W8bkeM0h5CFp+GcyfnhuOUE2L/ecwTE\n508OOUeR3j9cuU6H/qqRtzk=\n-----END PRIVATE KEY-----\n",
+    "client_email": "firebase-adminsdk-fbsvc@foodhouse-int.iam.gserviceaccount.com",
+    "client_id": "111045687796452436819",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40foodhouse-int.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+}
+
+// info bip secrets
+{"password":"bCIZtsE6aPPaS1xF8sYA7i7AMKvvEB","api_key":"e126bf0543387fc293ff60eedd00b6a0-520b6b7a-e4eb-44f1-99b2-9f40344ba62c","base_url":"https://8kq2kd.api.infobip.com"}
