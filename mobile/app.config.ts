8e82820d0cdad6d26f3c6d5e472f9304d84877a4
Rouclec <senatorasonganyi97@gmail.com>
2025-12-11 15:44:26 +0100

    backward compatibility for permissions

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -4,33 +4,37 @@ import * as dotenv from 'dotenv';
 // initialize dotenv
 dotenv.config();
 
-export default ({ config }: ConfigContext): ExpoConfig => ({
-  ...config,
-  name: 'Foodhouse',
-  slug: 'foodhouse',
-  ios: {
-    supportsTablet: true,
-    bundleIdentifier: 'com.foodhousecmr.foodhouse',
-    config: {
-      googleMapsApiKey: process.env.EXPO_PUBLIC_IOS_MAP_QUERY_KEY,
-    },
-  },
-  android: {
-    adaptiveIcon: {
-      foregroundImage: './assets/images/adaptive-icon.png',
-      backgroundColor: '#ffffff',
-    },
-    softwareKeyboardLayoutMode: 'pan',
-    package: 'com.foodhousecmr.foodhouse',
-    permissions: [
-      'android.permission.CAMERA',
-      'android.permission.ACCESS_FINE_LOCATION',
-    ],
-    edgeToEdgeEnabled: true,
-    config: {
-      googleMaps: {
-        apiKey: process.env.EXPO_PUBLIC_ANDROID_MAP_QUERY_KEY,
+export default function ({ config }: ConfigContext): ExpoConfig {
+  return {
+    ...config,
+    name: 'Foodhouse',
+    slug: 'foodhouse',
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: 'com.foodhousecmr.foodhouse',
+      config: {
+        googleMapsApiKey: process.env.EXPO_PUBLIC_IOS_MAP_QUERY_KEY,
       },
     },
-  },
-});
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/images/adaptive-icon.png',
+        backgroundColor: '#ffffff',
+      },
+      softwareKeyboardLayoutMode: 'pan',
+      package: 'com.foodhousecmr.foodhouse',
+      permissions: [
+        'android.permission.CAMERA',
+        'android.permission.ACCESS_FINE_LOCATION',
+        'android.permission.READ_MEDIA_IMAGES',
+        'android.permission.READ_MEDIA_VIDEO',
+      ],
+      edgeToEdgeEnabled: true,
+      config: {
+        googleMaps: {
+          apiKey: process.env.EXPO_PUBLIC_ANDROID_MAP_QUERY_KEY,
+        },
+      },
+    },
+  };
+}
